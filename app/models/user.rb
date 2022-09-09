f75caa1716967b38b5e8dad905aee6cc0045e34a
jodai <jodai0423@gmail.com>
2022-09-09 14:23:37 +0000

    [Start]応用課題７a

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :favorited_books, through: :favorites, source: :book
 
   has_one_attached :profile_image
 
